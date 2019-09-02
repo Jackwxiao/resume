bdb0a55297c0dcc589f4b1ee67805c0981630fac
wuxiao <1291383707@qq.com>
2019-09-02 10:32:05 +0800

    使用闭包和立即执行函数模块化代码

---
+++ js/auto-slide-up.js
@@ -0,0 +1,43 @@
+!function () {
+    // 添加 offset 类
+    let specialTags = document.querySelectorAll('[data-x]')
+    for (let i = 0; i < specialTags.length; i++) {
+        specialTags[i].classList.add('offset')
+    }
+
+    findClosestAndRemoveoOffset()
+    window.addEventListener('scroll', function (x) {
+        findClosestAndRemoveoOffset()
+    })
+
+    /*helper*/
+    function findClosestAndRemoveoOffset() {
+        let specialTags = document.querySelectorAll('[data-x]')
+        let minIndex = 0
+        for (let i = 1; i < specialTags.length; i++) {
+            if (Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)) {
+                minIndex = i
+            }
+        }
+        //minIndex 就是离窗口最近的元素
+        specialTags[minIndex].classList.remove('offset')
+        let id = specialTags[minIndex].id
+        let a = document.querySelector('a[href="#' + id + '"]')
+        let li = a.parentNode
+        let brothersAndMe = li.parentNode.children
+        for (let i = 0; i < brothersAndMe.length; i++) {
+            brothersAndMe[i].classList.remove('highlight')
+        }
+        li.classList.add('highlight')
+    }
+
+    let liTags = document.querySelectorAll('nav.menu > ul > li')
+    for (let i = 0; i < liTags.length; i++) {
+        liTags[i].onmouseenter = function (x) {
+            x.currentTarget.classList.add('active')
+        }
+        liTags[i].onmouseleave = function (x) {
+            x.currentTarget.classList.remove('active')
+        }
+    }
+}.call()
---
+++ js/init-swiper.js
@@ -0,0 +1,16 @@
+!function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+}.call()
