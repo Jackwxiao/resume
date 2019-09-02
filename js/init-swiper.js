b3511b86dea499029aa908581947b737d6f23f92
wuxiao <1291383707@qq.com>
2019-09-02 12:53:21 +0800

    进一步提升MVC思想

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,22 +1,28 @@
 !function () {
     var view = document.querySelector('#mySlides')
-    var controller = function (view) {
-        var mySwiper = new Swiper('.swiper-container', {
+    var controller = {
+        view: null,
+        swiper: null,
+        swiperOptions: {
             loop: true,
-
-            // If we need pagination
             pagination: {
                 el: '.swiper-pagination',
             },
-
-            // Navigation arrows
             navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
             },
-        })
+        },
+        init: function(view){
+          this.view = view
+          this.initSwiper()
+        },
+        initSwiper: function(){
+            this.swiper = new Swiper (
+                view.querySelector('.swiper-container'),
+                this.swiperOptions
+            )
+        }
     }
-    
-    controller(view)
-    
+    controller.init(view)
 }.call()
--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,13 +1,30 @@
 !function () {
     var view = document.querySelector('#topNavBar')
-    var controller = function(view){
-        window.addEventListener('scroll', function (x) {
+    var controller = {
+        view: null,
+        init: function(view){
+            this.view = view
+            this.bindEvents()
+            //this.bindEvents.call(this)
+        },
+        bindEvents: function(){
+            var view = this.view
+            window.addEventListener('scroll',  (x)=>{
             if (window.scrollY > 0) {
-                view.classList.add('sticky')
+                this.active()
             } else {
-                view.classList.remove('sticky')
+                this.deactive()
             }
         })
+        //箭头函数没有this，内 外的 this 是一样的,这里不用箭头函数会报错
+        },
+        active: function(){
+           this.view.classList.add('sticky') 
+        },
+        deactive: function(){
+           this.view.classList.remove('sticky') 
+        }
     }
-    controller(view)
+    controller.init(view)
+    //controller.init.call(controller,view)
 }.call()
